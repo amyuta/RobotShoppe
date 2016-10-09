29d8a6637e3c451e6944856595fd70529b7eefca
k-jorgemunoz <k.jorgemunoz@yahoo.com>
2016-10-09 18:23:45 -0500

    worked on the shoppe files
    I think that's all we need on the shoppe files

---
+++ shoppe.cpp
@@ -0,0 +1,33 @@
+#include "shoppe.h"
+
+void Shoppe::add_new_model(Robot_Model model) {
+  robot_models.push_back(model);
+}
+
+void Shoppe::add_order(Order order) {
+  orders.push_back(order);
+}
+
+void Shoppe::add_customer(Customer customer) {
+  customer.push_back(customer);
+}
+
+void Shoppe::add_sa(SalesAssociate sa) {
+  sales_associates.push_back(sa);
+}
+
+int Library::num_of_models() {
+  return robot_models.size();
+}
+
+int Library::num_of_orders() {
+  return orders.size();
+}
+
+int Library::num_of_customers() {
+  return customers.size();
+}
+
+int Library::num_of_sas() {
+  return sales_associates.size();
+}
--- shoppe.h
+++ shoppe.h
@@ -2,18 +2,28 @@
 #define SHOPPE_H
 
 #include "std_lib_facilities.h"
-#include "Head.h"
-#include "torso.h"
-
+#include "robot_model.h"
+#include "order.h"
+#include "customer.h"
+#include "sa.h"
 
 class Shoppe {
 public:
-	
+	void add_new_model(Robot_Model model);
+    void add_order(Order order);
+    void add_customer(Customer customer);
+    void add_sa(SalesAssociate sa);
+
+    int num_of_models;
+    int num_of_orders;
+    int num_of_customers;
+    int num_of_sas;
+
 private:
-	//vector<Robot_Model> robot_models;
-	//vector<Order> orders;
-	//vector<Customer> customers;
-	//vector<SalesAssociate> sales_associates;
+	vector<Robot_Model> robot_models;
+	vector<Order> orders;
+	vector<Customer> customers;
+	vector<SalesAssociate> sales_associates;
 };
 
 #endif
