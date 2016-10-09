cf8468f0944ac99ff69aaa0205b67f3305345a0b
farahaha <farah.abubaker@mavs.uta.edu>
2016-10-08 23:48:16 -0500

    Removing #include

--- shoppe.h
+++ shoppe.h
@@ -1,13 +1,10 @@
 #ifndef SHOPPE_H
 #define SHOPPE_H
 
-#include "view.h"
 #include "std_lib_facilities.h"
-#include "Arm.h"
 #include "Head.h"
 #include "torso.h"
-#include "controller.h"
-#include "shoppe.h"
+
 
 class Shoppe {
 public:
