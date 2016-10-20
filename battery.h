1c517cdcd0329a0f2d554fc46a3c1f21e07bdc19
amyuta <amy.barnett@mavs.uta.edu>
2016-10-19 21:19:32 -0500

    Final Version

--- battery.h
+++ battery.h
@@ -1,24 +1,19 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
-class Battery {
-    private:
-     double energy;
-     int model;
+//#include "std_lib_facilities.h"
+#include "RobotPart.h"
+#include "robot_model.h"
+
+class Battery : public RobotPart {
+    protected:
+    
+    int p_energy;
 
     public:
-     Battery(int type) : model(type)) { }
+     Battery(int part_num, double weight, double cost, string description, int energy, int quantity) : p_energy(energy), RobotPart(part_num, weight, cost, description, quantity){}
 
-     static const int battery1 = 1;
-     static const int battery2 = 2;
-     static const int battery3 = 3;
-
-       switch(model) {
-         case(battery1):return "battery1";
-         case(battery2):return "battery2";
-         case(battery3):return "battery3";
-
-       }
+	virtual void save_all(); 
 
  };
 
