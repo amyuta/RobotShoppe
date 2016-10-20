6f314d1c7e8bb4f541b44f132dbc8f4b58e4f4c1
amyuta <amy.barnett@mavs.uta.edu>
2016-10-19 22:19:17 -0500

    FINAL!!!!!!!!!! :)

--- customer.cpp
+++ customer.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "customer.h"
 #include <stdio.h>
 #include <fstream>
@@ -21,27 +20,3 @@ void Customer::list_all_detail(){
     
     cout << name << " " <<number;
 }
-=======
-#include "customer.h"
-#include <stdio.h>
-#include <fstream>
-#include <ostream>
-#include <iostream>
-using namespace std;
-
-void Customer::save_all(){
-
-    ofstream ofs;
-    ofs.open("data.txt", ofstream::out | ofstream::app);
-
-            ofs << number << endl;
-            ofs << name << endl;
-            ofs << sales_a << endl;
-
-            ofs.close();
-}
-void Customer::list_all_detail(){
-    
-    cout << name << " " <<number;
-}
->>>>>>> 4e08305d5fca81444807dc206782e19d17b66e75
